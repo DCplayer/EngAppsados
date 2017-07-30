5ae9da37c47080758992f11728c3da7aa6f743c4
arr15334 <arr15334@uvg.edu.gt>
2017-07-29 23:15:55 -0600

    Merge branch 'master' of https://github.com/DCplayer/EngAppsados
    # Conflicts:
    #	.idea/workspace.xml

--- EngappsadosApp/app/src/main/java/com/engappsados/engappsadosapp/MenuPrincipalActivity.java
+++ EngappsadosApp/app/src/main/java/com/engappsados/engappsadosapp/MenuPrincipalActivity.java
@@ -1,13 +1,31 @@
 package com.engappsados.engappsadosapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuPrincipalActivity extends AppCompatActivity {
+
+    public Button buttonReward;
+
+    public void init(){
+        buttonReward = (Button)findViewById(R.id.buttonReward);
+        buttonReward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent recompensa = new Intent(MenuPrincipalActivity.this, Rewards.class);
+                startActivity(recompensa);
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
+        init();
+
     }
 }
