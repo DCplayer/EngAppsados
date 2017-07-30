5ee414ce6366b5c4b32bd9b7726f74d1bd5979a4
JonnathanJuarez <jua15377@uvg.edu.gt>
2017-07-29 18:41:41 -0600

    cambio en unos comentarios

--- EngappsadosApp/app/src/main/java/com/engappsados/engappsadosapp/WelcomeActivity.java
+++ EngappsadosApp/app/src/main/java/com/engappsados/engappsadosapp/WelcomeActivity.java
@@ -32,14 +32,14 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Checking for first time launch - before calling setContentView()
+        // Revisa si es la primera vez que se usa la app - despues llama setContentView()
         prefManager = new PrefManager(this);
         if (!prefManager.isFirstTimeLaunch()) {
             launchHomeScreen();
             finish();
         }
 
-        // Making notification bar transparent
+        // haca la barraa de notificaciones transparente
         if (Build.VERSION.SDK_INT >= 21) {
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         }
