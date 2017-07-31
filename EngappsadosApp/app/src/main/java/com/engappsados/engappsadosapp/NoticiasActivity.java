add1085cc4133e18cd41d6c5c2507f59f9973a92
CoffeeBrewer <cas15151@uvg.edu.gt>
2017-07-30 19:19:38 -0600

    Codigo para modificar puntos
    Agregando el codigo de manejo de puntos en una de las actividades no
    diseñadas como prueba del programa ; D

--- EngappsadosApp/app/src/main/java/com/engappsados/engappsadosapp/NoticiasActivity.java
+++ EngappsadosApp/app/src/main/java/com/engappsados/engappsadosapp/NoticiasActivity.java
@@ -6,9 +6,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+
 public class NoticiasActivity extends AppCompatActivity implements View.OnClickListener {
     private TextView puntos;
     private Button botonMenos,botonMas;
+    private FirebaseUser usuario = FirebaseAuth.getInstance().getCurrentUser();
+    private DatabaseReference mDatabaseRef = FirebaseDatabase.getInstance().getReference();
+    private int points;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,11 +34,49 @@ public class NoticiasActivity extends AppCompatActivity implements View.OnClickL
     @Override
     public void onClick(View v) {
 
+        String Id = usuario.getUid();
+        Query query = mDatabaseRef.child(Id);
+        query.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                for(DataSnapshot data: dataSnapshot.getChildren()){
+
+                    int puntos = data.child("Puntos").getValue(Integer.class);
+                    points = puntos;
+                }
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
         if (R.id.btnMas == v.getId()) {
-            //do somethin
+
+            points = points + 1;
+            mDatabaseRef.child(usuario.getUid()).child("Puntos").setValue(points);
+
         }
         else if(R.id.btnMenos == v.getId()){
-            //do other thing
+            points = points - 1;
+            mDatabaseRef.child(usuario.getUid()).child("Puntos").setValue(points);
         }
 
     }
