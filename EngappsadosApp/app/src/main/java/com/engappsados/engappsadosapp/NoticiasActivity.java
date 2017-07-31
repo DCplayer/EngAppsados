89168c8cb2bf44cd209cf3dd8348591b6ce96425
JonnathanJuarez <jua15377@uvg.edu.gt>
2017-07-30 18:39:08 -0600

    botones temporales para prueba de contro de puntos de firebase

--- EngappsadosApp/app/src/main/java/com/engappsados/engappsadosapp/NoticiasActivity.java
+++ EngappsadosApp/app/src/main/java/com/engappsados/engappsadosapp/NoticiasActivity.java
@@ -2,12 +2,32 @@ package com.engappsados.engappsadosapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class NoticiasActivity extends AppCompatActivity {
-
+public class NoticiasActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView puntos;
+    private Button botonMenos,botonMas;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_noticias);
+        puntos = (TextView) findViewById(R.id.txt_cantPuntos);
+        botonMas = (Button) findViewById(R.id.btnMas);
+        botonMenos = (Button) findViewById(R.id.btnMenos);
+        botonMas.setOnClickListener(this);
+        botonMenos.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+
+        if (R.id.btnMas == v.getId()) {
+            //do somethin
+        }
+        else if(R.id.btnMenos == v.getId()){
+            //do other thing
+        }
+
     }
 }
