18c05fccf77eab8eb257198437b65091a0859c40
CoffeeBrewer <cas15151@uvg.edu.gt>
2017-08-16 01:37:55 -0600

    Arreglando problemas al interactuar con las actividades

--- EngappsadosApp/app/src/main/java/com/engappsados/engappsadosapp/Noticias_Tab.java
+++ EngappsadosApp/app/src/main/java/com/engappsados/engappsadosapp/Noticias_Tab.java
@@ -13,6 +13,7 @@ import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import android.support.v7.app.AppCompatActivity;
 
@@ -23,6 +24,28 @@ public class Noticias_Tab extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.activity_noticias, container, false);
+
+        //noticia1
+        TextView textView1 =(TextView)rootView.findViewById(R.id.leerMas);
+        textView1.setClickable(true);
+        textView1.setMovementMethod(LinkMovementMethod.getInstance());
+        String text1 = "<a href='https://www.guatemala.com/noticias/tecnologia/ingenieros-guatemaltecos-que-han-destacado-a-nivel-internacional.html'> Leer mas </a>";
+        textView1.setText(Html.fromHtml(text1));
+
+        //noticia2
+        TextView textView2 =(TextView)rootView.findViewById(R.id.leerMas2);
+        textView2.setClickable(true);
+        textView2.setMovementMethod(LinkMovementMethod.getInstance());
+        String text2 = "<a href='https://www.guatemala.com/noticias/tecnologia/guatemaltecos-participaron-como-conferencistas-en-droidcon-2017.html'> Leer mas </a>";
+        textView2.setText(Html.fromHtml(text2));
+
+        //noticia3
+        TextView textView3 =(TextView)rootView.findViewById(R.id.leerMas3);
+        textView3.setClickable(true);
+        textView3.setMovementMethod(LinkMovementMethod.getInstance());
+        String text3 = "<a href='https://www.guatemala.com/noticias/tecnologia/guatemaltecos-triunfan-en-competencia-de-seguridad-informatica-en-latinoamerica-2016.html'> Leer mas </a>";
+        textView3.setText(Html.fromHtml(text3));
+
 
         return rootView;
     }
