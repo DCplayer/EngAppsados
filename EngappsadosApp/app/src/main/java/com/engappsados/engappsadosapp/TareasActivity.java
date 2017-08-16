66d43ef9656189f0cabc1681839440745ebb0d35
CoffeeBrewer <cas15151@uvg.edu.gt>
2017-08-15 23:32:23 -0600

    Direccionando al usuario al Play Store

--- EngappsadosApp/app/src/main/java/com/engappsados/engappsadosapp/TareasActivity.java
+++ EngappsadosApp/app/src/main/java/com/engappsados/engappsadosapp/TareasActivity.java
@@ -1,15 +1,24 @@
 package com.engappsados.engappsadosapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ScrollView;
 import android.widget.TabWidget;
 import android.widget.TextView;
 
 public class TareasActivity extends AppCompatActivity {
     private TextView texto;
-    private ScrollView elListado;
+    private Button boton1;
+    private Button boton2;
+    private Button boton3;
+    private Button boton4;
+
+
 
 
     @Override
@@ -18,11 +27,58 @@ public class TareasActivity extends AppCompatActivity {
         setContentView(R.layout.activity_tareas);
 
         texto = (TextView)findViewById(R.id.textView3);
-        elListado = (ScrollView)findViewById(R.id.scrollView2);
+
+        boton1 = (Button)findViewById(R.id.button2);
+        boton2 = (Button)findViewById(R.id.button3);
+        boton3 = (Button)findViewById(R.id.button4);
+        boton4 = (Button)findViewById(R.id.button5);
+
+        boton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://play.google.com/store/apps/details?id=com.renap.pub&hl=es"));
+
+                startActivity(intent);
+            }
+        });
+
+        boton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://play.google.com/store/apps/details?id=gt.com.cs.mistermenu&hl=es"));
+
+                startActivity(intent);
+            }
+        });
+
+        boton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://play.google.com/store/apps/details?id=adn.guatemala.com&hl=es"));
+
+                startActivity(intent);
+            }
+        });
+
+        boton4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://play.google.com/store/apps/details?id=com.muniguate.consulta&hl=es"));
+
+                startActivity(intent);
+            }
+        });
 
 
 
 
 
     }
+
+
+
 }
