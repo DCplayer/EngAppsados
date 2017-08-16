006e7eb33e22a6664247d6fb88ee1b8f59941742
CoffeeBrewer <cas15151@uvg.edu.gt>
2017-08-15 21:27:31 -0600

    Creando las Tareas

--- EngappsadosApp/app/src/main/java/com/engappsados/engappsadosapp/TareasActivity.java
+++ EngappsadosApp/app/src/main/java/com/engappsados/engappsadosapp/TareasActivity.java
@@ -1,13 +1,28 @@
 package com.engappsados.engappsadosapp;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ScrollView;
+import android.widget.TabWidget;
+import android.widget.TextView;
 
 public class TareasActivity extends AppCompatActivity {
+    private TextView texto;
+    private ScrollView elListado;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tareas);
+
+        texto = (TextView)findViewById(R.id.textView3);
+        elListado = (ScrollView)findViewById(R.id.scrollView2);
+
+
+
+
+
     }
 }
